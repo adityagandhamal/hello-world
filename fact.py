e40d2b0c2aba68f4943e1cec4d4d4428457786b7
adity <adityagandhamal1327@gmail.com>
2020-03-11 13:25:34 +0530

    AP

--- fact.py
+++ fact.py
@@ -1,16 +1,19 @@
-n = int(input("Enter the length of AP: "))
+print("ARITHEMATIC PROGRESSION")
 
-def ap(n):
-    a = int(input("Enter the first term of the AP: "))
-    d = int(input("Enter the common difference of the AP: "))
+n = int(input("Enter the length of AP: "))   #for user to enter the length of the AP  
 
-    print("THE SEQUENCE IS:")
+def ap(n):                                   #defines function for AP  
+    a = int(input("Enter the first term of the AP: "))                 #for user to enter first term of the AP
+    d = int(input("Enter the common difference of the AP: "))          #for user to enter the common difference of the AP
 
-    print(a)
+    print("THE SEQUENCE IS:")                                          
 
-    for i in range(1,1+n):
+    print(a,end = " ")                            #prints the first term
+
+    for i in range(1,1+n):              #code for AP
         a+=d
-        print(a)
-
+        print(a,end = " ")
+        
+ap(n)                               #calling the function
 
 

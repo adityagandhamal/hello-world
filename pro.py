6d88bb603c8d3cc5508d6266df7351d6f9c0668c
adity <adityagandhamal1327@gmail.com>
2020-03-11 13:07:03 +0530

    comments added

--- pro.py
+++ pro.py
@@ -1,19 +1,19 @@
-from functools import reduce
+from functools import reduce                                                          #to use lambda function
 
-print("Enter the elements of the sequence")
-list_of_nums = list(map(int,input().split()))
-print(f"Your list: {list_of_nums}")
+print("Enter the elements of the sequence")                                           #input from the user
+list_of_nums = list(map(int,input().split()))                                         #user enters the numbers with spacing
+print(f"Your list: {list_of_nums}")           
 
-result = []
+result = []                                                                           #empty list
 
-c = 0
+c = 0                                                                                 #initialization
 
-for i in list_of_nums:
+for i in list_of_nums:                                                                #looping over given list of numbers from the user
 
-    list_of_nums.remove(i)
-    r = reduce(lambda x,y:x*y,list_of_nums)
-    result.append(r)
-    list_of_nums.insert(c,i)
-    c+=1
+    list_of_nums.remove(i)                                                            #removes the element for each iteration i
+    r = reduce(lambda x,y:x*y,list_of_nums)                                           #anonymous function 
+    result.append(r)                                                                  #returned value of lambda function is appended to the empty list
+    list_of_nums.insert(c,i)                                                          #inserts the removed iteration i
+    c += 1                                                                            #incrementation
 
-print(f"Your answer: {result}")
+print(f"Your answer: {result}")                                                       #final output answer

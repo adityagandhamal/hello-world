7ac98937f7c7e724effac66c1ec92ba72923abfa
adityagandhamal <61016383+adityagandhamal@users.noreply.github.com>
2020-03-11 20:21:16 +0530

    Update fib.py

--- fib.py
+++ fib.py
@@ -1,15 +1,15 @@
-n = int(input("Enter the length of the series: "))
+n = int(input("Enter the length of the series: "))   #for user to input length of the fibonacci series
 
-print("The Fibonacci Series is: ")
+print("The Fibonacci Series is: ")                   
 
-a = 0
+a = 0                                               '''first term and second terms of the fibonacci series'''
 b = 1
 
-print(a,end=" ")
+print(a,end=" ")                                    #prints first and second terms
 print(b,end=" ")
 
-for i in range(n-1):
-    c = a + b
-    a = b
-    b = c
-    print(c,end=" ")
+for i in range(n-1):                                #looping over the length provided by the user
+    c = a + b                                       #third variable which is the next term of the fibonacci series
+    a = b                                           '''value of a is assigned to b,'''
+    b = c                                           '''and value of b is assigned to c'''
+    print(c,end=" ")                                #prints value of c and the loop continues
